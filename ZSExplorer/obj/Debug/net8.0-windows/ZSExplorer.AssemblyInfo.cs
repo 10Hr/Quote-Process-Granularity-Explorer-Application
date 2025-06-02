cc8a7ac734b75742cde10773db13a41b11f773c7
10Hr <10hourmail@gmail.com>
2025-06-02 18:18:42 -0400

    Updated Release

--- ZSExplorer/obj/Debug/net8.0-windows/ZSExplorer.AssemblyInfo.cs
+++ ZSExplorer/obj/Debug/net8.0-windows/ZSExplorer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ZSExplorer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51fbf8e6439b3485d6062f1919e97b2848013a7b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2999d96104129077b3062f1cd1bda12b1db0305")]
 [assembly: System.Reflection.AssemblyProductAttribute("ZSExplorer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ZSExplorer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
